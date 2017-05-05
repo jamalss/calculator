b73208eec304054d64aea8960e7d5c0c7e87e845
Jamal Samuel <jamal8010@gmail.com>
2017-05-05 12:25:42 -0400

    modify js/scripts.js file and add README.md file

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,35 @@
-/*var add = function(number1, number2) {
+var add = function(number1, number2) {
   return number1 + number2;
 };
 
 var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: "));
 var result = add(number1, number2);
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
-var result = subtract(number1, number2);
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
-var result = multiply(number1, number2);
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
-var result = divide(number1, number2);
-
 alert(result);
 
+/*var subtract = function(number3, number4) {
+  return number3 - number4;
+};
+
+var number3 = parseInt(prompt("Enter a number: "));
+var number4 = parseInt(prompt("Enter another number: "));
+var result = subtract(number3, number4);
+
+var multiply = function(number5, number6) {
+  return number5 * number6;
+};
+
+var number5 = parseInt(prompt("Enter a number: "));
+var number6 = parseInt(prompt("Enter another number: "));
+var result = multiply(number5, number6);
+
+var divide = function(number7, number8) {
+  return number7 / number8;
+};
+
+var number7 = parseInt(prompt("Enter a number: "));
+var number8 = parseInt(prompt("Enter another number: "));
+var result = divide(number7, number8);
+
+alert(result);
 */
-
-$(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-});
